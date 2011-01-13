18807c243abb8ceadc4450c814a13bc611de239e
zolkko <zolkko@gmail.com>
2011-01-13 20:50:11 +0300

    Make file for windows is added

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
-#include <avr/io.h>
+#define F_CPU 25000000U
+
+#include <avr/io.h> 
+#include <util/delay.h> 
 
 
 int main(void)
 {
-    // TODO: Initialize RTC
-    // TODO: Initialize DISPLAY
-    // TODO: Initialize SD Card
-    
     return 0;
 }
+
