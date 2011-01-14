2253ba7b016683a7a1ea1ff8d145792193a65297
zolkko <zolkko@gmail.com>
2011-01-14 11:59:49 +0300

    Initialization of rtc

--- src/rtc_if.c
+++ src/rtc_if.c
@@ -18,6 +18,14 @@ void rtc_init(void)
     CLK.RTCCTRL = CLK_RTCSRC_RCOSC_gc | CLK_RTCEN_bm;
     
     // RTC prescaller
+    RTC.CNT  = 0;
+    RTC.COMP = 0; // ???
+    RTC.PER  = 0; // ???
     RTC.CTRL = (RTC.CTRL & ~RTC_PRESCALER_gm) | RTC_PRESCALER_DIV1_gc;
+
+    // Setting RTC Interrupt overflow level
+    // TODO: RTC_OFVINTLVL_gm | RTC_COMPINTLVL_gm
+    RTC.INTCTRL = (RTC.INTCTRL & ~RTC_OVFINTLVL_gm) | 1;
+    RTC.INTCTRL = (RTC.INTCTRL & ~RTC_COMPINTLVL_gm) | 0; // Do not use this kind of interupt
 }
 
